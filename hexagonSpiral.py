1712cf3b0abcee433543422c94a0c527e5921933
Niranjan8373 <83953720+Niranjan8373@users.noreply.github.com>
2021-06-03 16:36:40 +0530

    Add files via upload

---
+++ hexagonSpiral.py
@@ -0,0 +1,10 @@
+import turtle
+
+colors = ["red","blue","yellow","purple","green","orange"]
+hexagon = turtle.Pen()
+turtle.bgcolor("black")
+for i in range(360):
+    hexagon.pencolor(colors[i%6])
+    hexagon.width(i/100+1)
+    hexagon.forward(i)
+    hexagon.left(59)
